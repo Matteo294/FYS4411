6b8f3ad5d27866391feb23779c72bcb5395720f7
Matteo294 <zmatteo.29@gmail.com>
2021-01-29 17:08:35 +0100

    Local enery rewritten

--- Stuff/harmonic_oscilator_obj_or/spherical.cpp
+++ Stuff/harmonic_oscilator_obj_or/spherical.cpp
@@ -16,29 +16,15 @@ double Spherical::potential(){
 
 double Spherical::LocalEnergy(){
 
-    // things I need multiple times in the calculations: better calculate them once for all
+    // things I need multiple times in the calculations or long expressions: better calculate them once for all
     double alpha = this->system->getWavefunction()->getParameter(0);
     double mass = 1.0; // !!!!!!!!!!!!!!!! Hard-code this is true only for the chosen model
     double res = 0.0;
     double psi = this->system->getWavefunction()->evaluate();
     double psidotdot = this->system->getWavefunction()->evaluateSecondDerivative();
 
-    // potential contribution
-    for(int i=0; i<this->system->getNParticles() ; i++){    
-        for(int j=0; j<this->system->getDimension(); j++){
-            res = res + pow( this->system->particles[i]->position[j], 2);
-        }
-    } 
+    // Local energy
+    double EL = 1/psi * ((-0.5)*psidotdot + this->potential()*psi);
 
-    res = res * (0.5 * pow(this->omega,2) * mass - 2*pow(alpha, 2) / mass);
-    res = res + this->system->getNParticles() * this->system->getDimension() * alpha/mass;
-
-    double EL2 = 1/psi * ((-0.5)*psidotdot + this->potential()*psi);
-
-    cout << res << " " << EL2 << endl;
-
-    return res;
-
-    // second derivative contribution
-    //res = res - (0.5/this->system->particles[0]->mass/this->system->getWavefunction()->evaluate())*this->system->getWavefunction()->evaluateSecondDerivative();
+    return EL;
 }
