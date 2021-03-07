f4da645525462a89cbf7ffb322396903c4c98f4e
Matteo294 <zmatteo.29@gmail.com>
2021-03-07 18:50:24 +0100

    varius modifications

---
+++ Project1/code/plotting/alpha_analysis.py
@@ -0,0 +1,15 @@
+import numpy as np 
+from matplotlib import pyplot as plt 
+import pandas as pd
+import matplotlib
+
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+
+data = pd.read_csv('./data/varying_alpha.csv')
+
+plt.plot(data['alpha'], data['energy'], linewidth=1.8, color='black')
+plt.xlabel(r'$\alpha$', fontsize=14)
+plt.ylabel('E / N', fontsize=14)
+plt.grid()
+plt.show()
